09dff0922f798051b31dc24c93ca6edbd361d7c0
Solange <sol.sch1990@gmail.com>
2019-11-26 11:45:29 +0100

    Change Sprites Owls

--- Assets/Scripts/Enemy/Owl.cs
+++ Assets/Scripts/Enemy/Owl.cs
@@ -10,16 +10,27 @@ public class Owl : MonoBehaviour
 
     [SerializeField] Transform targetChase;
 
+    [SerializeField] Vector3 leftOffset;
+    [SerializeField] Vector3 rightOffset;
+
+    Vector3 leftTarget;
+    Vector3 rightTarget;
+
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        leftTarget = transform.position + leftOffset;
+        rightTarget = transform.position + rightOffset;
     }
 
     
     void Update()
     {
-        Vector3 velocity = (targetChase.position - transform.position).normalized * speed;
+        Vector3 velocity = (leftTarget - transform.position).normalized * speed;
         velocity = new Vector3(velocity.x, body.velocity.y, 0);
+
+        body.velocity = velocity;
         Debug.Log("Hello");
+       // Destroy(gameObject);
     }
 }
